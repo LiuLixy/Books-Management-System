e4d596807827ecf50f66c17eac9c6eda07287d8f
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-07-25 12:02:27 +0800

    图书类别

---
+++ books-management/src/main/java/com/lwstudy/booksmanagement/common/BookType.java
@@ -0,0 +1,30 @@
+package com.lwstudy.booksmanagement.common;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/7/24 10:00
+ */
+public enum BookType {
+    // 哲学
+    Philosophy,
+    // 社会科学
+    SocialScience,
+    // 政治、法律
+    PoliticalLaw,
+    // 军事
+    Military,
+    // 经济
+    Economy,
+    // 语言
+    Language,
+    // 文学
+    Literature,
+    // 历史
+    History,
+    // 数理科学
+    MathematicalScience,
+    // 计算机科学
+    ComputerScience,
+    // 综合类
+    Comprehensive
+}
