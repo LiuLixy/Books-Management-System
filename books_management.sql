febc9105b602bdefcb8655b6576f2c0a947f65ca
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-07-30 19:29:34 +0800

    Update books_management.sql

--- books_management.sql
+++ books_management.sql
@@ -1,39 +1,139 @@
-drop database if exists `books_management`;
-create database if not exists `books_management` default character set utf8 collate utf8_general_ci;
+-- MySQL dump 10.13  Distrib 5.7.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: books_management
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
 
-use books_management;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 读者信息表
-drop table if exists `user_info`;
-create table if not exists `user_info` (
-	user_id varchar(32) primary key comment '读者学号',
-	user_name varchar(32) not null comment '读者姓名',
-	gender varchar(8) not null comment '读者性别', 
-	telephone varchar(20) not null comment '读者联系方式',
-	institute varchar(32) comment '读者所在院系',
-	user_class varchar(32) not null comment '读者所在班级',
-	book_card_id varchar(32) not null comment '图书证号'
-)engine innodb;
+--
+-- Table structure for table `admin`
+--
 
--- 图书信息表
-drop table if exists `book_info`;
-create table if not exists `book_info` (
-	book_id varchar(32) primary key comment '图书编号',
-	book_name varchar(32) not null comment '书名',
-	book_type varchar(32) not null comment '图书类别',
-	author varchar(32) not null comment '作者',
-	publishing_house varchar(32) not null comment '出版社',
-	price float(5, 2) unsigned comment '图书价格'  
-)engine innodb;
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- 图书借阅表
-drop table if exists `borrow_info`;
-create table if not exists `borrow_info` (
-	book_id varchar(32) primary key comment '图书编号',
-	book_card_id varchar(32) not null comment '图书证号',
-	user_id varchar(32) not null comment '读者学号',
-	user_name varchar(32) not null comment '读者姓名',
-	borrow_time datetime not null comment '借书日期',
-	deadline_time datetime not null comment '还书期限',
-	is_renew char(1) not null comment '是否续借, 0:不续借, 1:续借'
-)engine innodb;
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1001,'admin','admin');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book_info`
+--
+
+DROP TABLE IF EXISTS `book_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_info` (
+  `book_id` varchar(32) NOT NULL COMMENT '图书编号',
+  `book_name` varchar(32) NOT NULL COMMENT '书名',
+  `book_type` varchar(32) NOT NULL COMMENT '图书类别',
+  `author` varchar(32) NOT NULL COMMENT '作者',
+  `publishing_house` varchar(32) DEFAULT NULL COMMENT '出版社',
+  `price` float(5,2) unsigned DEFAULT NULL COMMENT '图书价格',
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_info`
+--
+
+LOCK TABLES `book_info` WRITE;
+/*!40000 ALTER TABLE `book_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `borrow_info`
+--
+
+DROP TABLE IF EXISTS `borrow_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `borrow_info` (
+  `borrow_id` varchar(32) NOT NULL COMMENT '借阅编号',
+  `book_id` varchar(32) NOT NULL COMMENT '图书编号',
+  `user_id` varchar(32) NOT NULL COMMENT '读者学号',
+  `user_name` varchar(32) NOT NULL COMMENT '读者姓名',
+  `borrow_time` datetime NOT NULL COMMENT '借书日期',
+  `deadline_time` datetime NOT NULL COMMENT '还书期限',
+  `is_renew` char(1) NOT NULL COMMENT '是否续借, 0:不续借, 1:续借',
+  PRIMARY KEY (`borrow_id`),
+  KEY `FK_ID` (`user_id`),
+  KEY `fk_book_id` (`book_id`),
+  CONSTRAINT `FK_ID` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`),
+  CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `book_info` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrow_info`
+--
+
+LOCK TABLES `borrow_info` WRITE;
+/*!40000 ALTER TABLE `borrow_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `borrow_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `user_id` varchar(32) NOT NULL COMMENT '读者学号',
+  `user_name` varchar(32) NOT NULL COMMENT '读者姓名',
+  `gender` varchar(8) NOT NULL COMMENT '读者性别',
+  `telephone` varchar(20) NOT NULL COMMENT '读者联系方式',
+  `institute` varchar(32) DEFAULT NULL COMMENT '读者所在院系',
+  `user_class` varchar(32) NOT NULL COMMENT '读者所在班级',
+  `book_card_id` varchar(32) NOT NULL COMMENT '图书证号',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-30 19:27:30
